c9ad3caa69cb7cb9ddffdecde3849de109df2cec
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-08-08 18:22:04 +0900

    use uuid method to define the UUID type

--- db/migrate/20150804223422_create_join_table_deity_shrine.rb
+++ db/migrate/20150804223422_create_join_table_deity_shrine.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableDeityShrine < ActiveRecord::Migration
   def change
     create_table :deities_shrines, id: false do |t|
-      t.column :deity_id, :uuid, null: false
-      t.column :shrine_id, :uuid, null: false
+      t.uuid :deity_id, null: false
+      t.uuid :shrine_id, null: false
     end
   end
 end
