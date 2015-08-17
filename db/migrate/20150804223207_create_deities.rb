41776b8fcd322c00248b683679c702e755d4ecde
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-08-18 07:53:22 +0900

    add dewa data

--- db/migrate/20150804223207_create_deities.rb
+++ db/migrate/20150804223207_create_deities.rb
@@ -1,5 +1,6 @@
 class CreateDeities < ActiveRecord::Migration
   def change
+    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
     create_table :deities, id: :uuid, default: 'gen_random_uuid()' do |t|
       t.string :name
       t.string :name_hiragana
