2710843f3915f7868a8c2feebaa8f94307e541b2
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-11-06 06:53:08 +0900

    add admin files

---
+++ app/dashboards/shrine_dashboard.rb
@@ -0,0 +1,57 @@
+require "administrate/base_dashboard"
+
+class ShrineDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    deities: Field::HasMany,
+    id: Field::String,
+    name: Field::String,
+    name_hiragana: Field::String,
+    name_katakana: Field::String,
+    detail: Field::Text,
+    longitude: Field::Number.with_options(decimals: 2),
+    latitude: Field::Number.with_options(decimals: 2),
+    address: Field::String,
+    hp: Field::String,
+    tags: Field::Text,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime,
+  }
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = [
+    :deities,
+    :id,
+    :name,
+    :name_hiragana,
+  ]
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = ATTRIBUTE_TYPES.keys
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = [
+    :deities,
+    :name,
+    :name_hiragana,
+    :name_katakana,
+    :detail,
+    :longitude,
+    :latitude,
+    :address,
+    :hp,
+    :tags,
+  ]
+end
